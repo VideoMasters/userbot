457c8e2ec7951eb15258d899fa9cb5d09dfa9d3e
* <rkdeshdeepak1@gmail.com>
2020-06-13 21:01:28 +0530

    Renamed ubot.py __main__.py

--- ubot.py
+++
@@ -1,8 +0,0 @@
-import asyncio,os
-from pyrogram import Client 
-api_id=int(os.environ['API_ID'])
-api_hash=os.environ['API_HASH']
-HU_STRING_SESSION=os.environ['HU_STRING_SESSION']
-uBot = Client(HU_STRING_SESSION,api_id,api_hash,plugins=dict(root="plugins"))
-if __name__ == "__main__" :
-    uBot.run()
